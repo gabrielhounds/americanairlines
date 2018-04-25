d88b6c057f1e4d3f27b2eeb6319b66332f67379e
Patrick Batey <pbatey@patrickBatey-macBook.fios-router.home>
2018-04-24 21:42:40 -0400

    Updates

--- main.js
+++ main.js
@@ -3,14 +3,22 @@ $(document).ready(function(){
 });
 
 function init() {
+	var t = TweenMax;
+
 	console.log('init');
 
-	var panorama, viewer, infospot1, infospot2, infospot3;
+	var intro = $('#intro');
+	var overlay1 = $('#overlay1');
+	var overlay2 = $('#overlay2');
+	var overlay3 = $('#overlay3');
 
+	t.set('#main', {autoAlpha:0});
+	t.set([intro, overlay1, overlay2, overlay3], {autoAlpha:0});
+
+	var panorama, viewer, infospot1, infospot2, infospot3;
 	var main = document.getElementById('pano');
 
 	panorama = new PANOLENS.ImagePanorama( 'aa_pano_50.jpg' );
-
 
 	infospot1 = new PANOLENS.Infospot( 1000, 'hotspot1.png');
 	infospot1.position.set( 3368.82, -3505.08, 1154.51 );
@@ -29,19 +37,28 @@ function init() {
 	panorama.add( infospot2 );
 	panorama.add( infospot3 );
 
-	//infospot1.addEventListener( 'click', function(){ alert('CLICK'); });
-	//infospot1.addEventListener( 'click', function(){ alert('CLICK'); });
 
 	$(infospot1).click(function(){
-		alert('CLICK 1');
+		//alert('CLICK 1');
+		t.set('#main', {autoAlpha:1});
+		t.to(overlay1, 0.3, {autoAlpha:1, ease:Power2.easeOut});
 	});
 
 	$(infospot2).click(function(){
-		alert('CLICK 2');
+		//alert('CLICK 2');
+		t.set('#main', {autoAlpha:1});
+		t.to(overlay2, 0.3, {autoAlpha:1, ease:Power2.easeOut});
 	});
 
 	$(infospot3).click(function(){
-		alert('CLICK 3');
+		//alert('CLICK 3');
+		t.set('#main', {autoAlpha:1});
+		t.to(overlay3, 0.3, {autoAlpha:1, ease:Power2.easeOut});
+	});
+
+	$('.overlay .close').click( function(){
+		t.set('#main', {autoAlpha:0});
+		t.set('.overlay', {autoAlpha:0});
 	});
 
 
@@ -49,10 +66,12 @@ function init() {
 		container: main,
 		cameraFov: 70,
 		autoHideInfospot: false,
+		controlBar: false,
 		output: 'console'
 	});
 
 	viewer.add( panorama );
+	//viewer.enableControl( PANOLENS.Controls.DEVICEORIENTATION )
 	viewer.getControl().rotateLeft(-90 * Math.PI / 180);
     viewer.getControl().update();
 
